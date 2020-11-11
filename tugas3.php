6e0d8dc0f75bdbfd03fdc9da58fd407f8de99333
defanggaabypn <onecircle.24@gmail.com>
2020-11-11 17:43:57 +0700

    Praktikum Minggu Ke 2

---
+++ tugas3.php
@@ -0,0 +1,24 @@
+<?php
+function prima($n){
+  
+  for($i=1;$i<=$n;$i++){  
+          
+          $counter = 0; 
+          for($j=1;$j<=$i;$j++){ 
+              
+         
+                if($i % $j==0){ 
+                    
+                      $counter++;
+                }
+          }
+        
+        if($counter==2){
+             
+               print $i." adalah bilangan prima <br/>";
+        }
+ }
+} 
+
+prima(40)
+?>
